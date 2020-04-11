a0ea998f56d6200495df3d0eeecd0297dffbe64f
Panagiotis Sotiropoulos <psotirop@redhat.com>
2020-04-11 10:33:13 +0200

    Separating tests ... Closing a test version set for a server.

--- src/java/main/org/jboss/additional/testsuite/jdkall/present/others/NettyHeaderTest.java
+++ src/java/main/org/jboss/additional/testsuite/jdkall/present/others/NettyHeaderTest.java
@@ -18,6 +18,7 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.jboss.eap.additional.testsuite.annotations.EapAdditionalTestsuite;
+import org.jboss.eap.additional.testsuite.annotations.ATTest;
 import org.junit.Test;
 
 @RunWith(Arquillian.class)
@@ -50,7 +51,10 @@ public class NettyHeaderTest {
         assertTrue(request.decoderResult().isFailure());
         assertTrue(request.decoderResult().cause() instanceof IllegalArgumentException);
         assertFalse(channel.finish());
-
+    }
+    
+    @ATTest({"modules/testcases/jdkAll/Wildfly/others/src/main/java#19.0.0.Beta2*20.0.0.Alpha2","modules/testcases/jdkAll/Eap7/others/src/main/java#7.3.1.GA","modules/testcases/jdkAll/Eap72x/others/src/main/java#7.2.7","modules/testcases/jdkAll/Eap72x-Proposed/others/src/main/java#7.2.7"})
+    public void testNettyHeader2() {
         String requestStr2 = "POST / HTTP/1.1\r\n" +
                 "Host: example.com\r\n" +
                 "Connection: close\r\n" +
@@ -58,7 +62,7 @@ public class NettyHeaderTest {
                 "Transfer-Encoding: chunked\r\n\r\n" +
                 "0\r\n\r\n";
 
-        channel = new EmbeddedChannel(new HttpRequestDecoder());
+        EmbeddedChannel channel = new EmbeddedChannel(new HttpRequestDecoder());
         assertTrue(channel.writeInbound(Unpooled.copiedBuffer(requestStr2, CharsetUtil.US_ASCII)));
 
         HttpRequest request2 = channel.readInbound();
